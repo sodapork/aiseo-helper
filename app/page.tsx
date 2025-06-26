f78ab2be2074f2ac41cb0850f340ee4b082f5b48
John Popham <john@gocanopy.com>
2025-06-26 14:28:38 -0400

    Move ToolsGrid above Features on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Header from '@/components/Header'
 import Hero from '@/components/Hero'
-import Features from '@/components/Features'
 import ToolsGrid from '@/components/ToolsGrid'
+import Features from '@/components/Features'
 import Footer from '@/components/Footer'
 
 export default function Home() {
@@ -9,8 +9,8 @@ export default function Home() {
     <main className="min-h-screen">
       <Header />
       <Hero />
-      <Features />
       <ToolsGrid />
+      <Features />
       <Footer />
     </main>
   )
